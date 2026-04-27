a1f64340fdbe517c63c20553ad75dd153c604b2f
MPB <matheuspaivab2013@gmail.com>
2026-04-26 21:27:16 -0300

    Add files via upload

---
+++ Exercício 2 – Classe ContaBancaria/Exercício 2 – Classe ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class ContaBancaria
+{
+    public string NumeroConta;
+    public string NomeTitular;
+    public double Saldo;
+
+    public void ReceberDados()
+    {
+        Console.Write("Digite o número da conta: ");
+        NumeroConta = Console.ReadLine();
+
+        Console.Write("Digite o nome do titular: ");
+        NomeTitular = Console.ReadLine();
+
+        Console.Write("Digite o saldo inicial: ");
+        Saldo = double.Parse(Console.ReadLine());
+    }
+
+    public void Depositar()
+    {
+        Console.Write("Digite o valor para depósito: ");
+        double valor = double.Parse(Console.ReadLine());
+
+        Saldo += valor;
+        Console.WriteLine("Depósito realizado com sucesso!");
+    }
+
+    public void Sacar()
+    {
+        Console.Write("Digite o valor para saque: ");
+        double valor = double.Parse(Console.ReadLine());
+
+        if (valor <= Saldo)
+        {
+            Saldo -= valor;
+            Console.WriteLine("Saque realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente!");
+        }
+    }
+
+    public void MostrarSaldo()
+    {
+        Console.WriteLine("\n=== DADOS DA CONTA ===");
+        Console.WriteLine("Conta: " + NumeroConta);
+        Console.WriteLine("Titular: " + NomeTitular);
+        Console.WriteLine("Saldo: R$ " + Saldo.ToString("F2"));
+    }
+}
---
+++ Exercício 2 – Classe ContaBancaria/Exercício 2 – Classe ContaBancaria/Exercício 2 – Classe ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria();
+
+        conta.ReceberDados();
+
+        int opcao;
+
+        do
+        {
+            Console.WriteLine("\n===== MENU =====");
+            Console.WriteLine("1 - Depositar");
+            Console.WriteLine("2 - Sacar");
+            Console.WriteLine("3 - Mostrar Saldo");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    conta.Depositar();
+                    break;
+
+                case 2:
+                    conta.Sacar();
+                    break;
+
+                case 3:
+                    conta.MostrarSaldo();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Encerrando...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+
+        Console.ReadKey();
+    }
+}
