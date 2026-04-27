0b662b949cbdaaaf0b2139cbfd8c53f77be1ef32
MPB <matheuspaivab2013@gmail.com>
2026-04-26 21:28:03 -0300

    Add files via upload

---
+++ Exercício 3 – Classe Produto/Exercício 3 – Classe Produto/Produto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Produto
+{
+    public string CodigoProduto;
+    public string NomeProduto;
+    public double Preco;
+    public int QuantidadeEstoque;
+
+
+    public void ReceberDados()
+    {
+        Console.Write("Digite o código do produto: ");
+        CodigoProduto = Console.ReadLine();
+
+        Console.Write("Digite o nome do produto: ");
+        NomeProduto = Console.ReadLine();
+
+        Console.Write("Digite o preço: ");
+        Preco = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a quantidade em estoque: ");
+        QuantidadeEstoque = int.Parse(Console.ReadLine());
+    }
+
+
+    public void AdicionarEstoque()
+    {
+        Console.Write("Digite a quantidade para adicionar: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        QuantidadeEstoque += quantidade;
+        Console.WriteLine("Estoque atualizado com sucesso!");
+    }
+
+ 
+    public void RemoverEstoque()
+    {
+        Console.Write("Digite a quantidade para remover: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        if (quantidade <= QuantidadeEstoque)
+        {
+            QuantidadeEstoque -= quantidade;
+            Console.WriteLine("Remoção realizada com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Estoque insuficiente!");
+        }
+    }
+
+    public void MostrarProduto()
+    {
+        Console.WriteLine("\n=== DADOS DO PRODUTO ===");
+        Console.WriteLine("Código: " + CodigoProduto);
+        Console.WriteLine("Nome: " + NomeProduto);
+        Console.WriteLine("Preço: R$ " + Preco.ToString("F2"));
+        Console.WriteLine("Estoque: " + QuantidadeEstoque);
+    }
+}
---
+++ Exercício 3 – Classe Produto/Exercício 3 – Classe Produto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto produto = new Produto();
+
+        produto.ReceberDados();
+
+        int opcao;
+
+        do
+        {
+            Console.WriteLine("\n===== MENU =====");
+            Console.WriteLine("1 - Adicionar estoque");
+            Console.WriteLine("2 - Remover estoque");
+            Console.WriteLine("3 - Mostrar produto");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    produto.AdicionarEstoque();
+                    break;
+
+                case 2:
+                    produto.RemoverEstoque();
+                    break;
+
+                case 3:
+                    produto.MostrarProduto();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Encerrando...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+
+        Console.ReadKey();
+    }
+}
