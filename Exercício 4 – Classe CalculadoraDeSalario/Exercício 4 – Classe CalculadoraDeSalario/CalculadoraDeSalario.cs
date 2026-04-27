fb899b6bead14e7296211dc5e1decf1fb37f7e39
MPB <matheuspaivab2013@gmail.com>
2026-04-26 21:28:36 -0300

    Add files via upload

---
+++ Exercício 4 – Classe CalculadoraDeSalario/Exercício 4 – Classe CalculadoraDeSalario/CalculadoraDeSalario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class CalculadoraDeSalario
+{
+    public string NomeFuncionario;
+    public double SalarioBase;
+    public double SalarioFinal;
+
+    public void ReceberDados()
+    {
+        Console.Write("Digite o nome do funcionário: ");
+        NomeFuncionario = Console.ReadLine();
+
+        Console.Write("Digite o salário base: ");
+        SalarioBase = double.Parse(Console.ReadLine());
+
+    
+        SalarioFinal = SalarioBase;
+    }
+
+
+    public void CalcularAumento(double percentual)
+    {
+        double aumento = SalarioFinal * (percentual / 100);
+        SalarioFinal += aumento;
+
+        Console.WriteLine("Aumento aplicado!");
+    }
+
+
+    public void CalcularDesconto(double percentual)
+    {
+        double desconto = SalarioFinal * (percentual / 100);
+        SalarioFinal -= desconto;
+
+        Console.WriteLine("Desconto aplicado!");
+    }
+
+    public void MostrarSalario()
+    {
+        Console.WriteLine("\n=== DADOS DO FUNCIONÁRIO ===");
+        Console.WriteLine("Nome: " + NomeFuncionario);
+        Console.WriteLine("Salário Base: R$ " + SalarioBase.ToString("F2"));
+        Console.WriteLine("Salário Final: R$ " + SalarioFinal.ToString("F2"));
+    }
+}
---
+++ Exercício 4 – Classe CalculadoraDeSalario/Exercício 4 – Classe CalculadoraDeSalario/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculadoraDeSalario funcionario = new CalculadoraDeSalario();
+
+        funcionario.ReceberDados();
+
+        int opcao;
+        double percentual;
+
+        do
+        {
+            Console.WriteLine("\n===== MENU =====");
+            Console.WriteLine("1 - Aplicar aumento");
+            Console.WriteLine("2 - Aplicar desconto");
+            Console.WriteLine("3 - Mostrar salário");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.Write("Digite o percentual de aumento: ");
+                    percentual = double.Parse(Console.ReadLine());
+                    funcionario.CalcularAumento(percentual);
+                    break;
+
+                case 2:
+                    Console.Write("Digite o percentual de desconto: ");
+                    percentual = double.Parse(Console.ReadLine());
+                    funcionario.CalcularDesconto(percentual);
+                    break;
+
+                case 3:
+                    funcionario.MostrarSalario();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Encerrando...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+        } while (opcao != 0);
+
+        Console.ReadKey();
+    }
+}
